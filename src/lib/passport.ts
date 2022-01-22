38b0f52bff2d943d3cb2c8c3d4c88840748fb345
Isaac AC <sacntpr@gmail.com>
2022-01-22 15:48:38 +0100

    ok

--- src/lib/passport.ts
+++ src/lib/passport.ts
@@ -1,76 +1,83 @@
-import { Request } from "express"
-import {encryptPassword, decryptPassword} from "../lib/helpers";
-
+import { Request } from "express";
+import { encryptPassword, decryptPassword } from "../lib/helpers";
 
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import { IUser } from "../interfaces/register/user.interface";
-import {connect} from "../connect_database";
+import { connect } from "../connect_database";
 
+passport.use(
+  "signup",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (req: Request, username: string, password: string, done: any) => {
+      const { fullname, email, repeatpassword } = req.body;
 
-passport.use("signup", new LocalStrategy({
-    usernameField: "username",
-    passwordField: "password",
-    passReqToCallback: true
-}, async (req: Request, username: string, password: string, done:any )=>{
-    const {fullname, email, repeatpassword} = req.body;
-
-    if(password === repeatpassword){
+      if (password === repeatpassword) {
         const db = await connect();
         password = await encryptPassword(password);
         let newUser: IUser = {
-            fullname,
-            username,
-            email,
-            password
+          fullname,
+          username,
+          email,
+          password,
         };
-        const result:any = await db.query("INSERT INTO users SET ?", [newUser]);
+        const result: any = await db.query("INSERT INTO users SET ?", [
+          newUser,
+        ]);
         console.log("USER ADD ");
         newUser.id = result[0].insertId;
         return done(null, newUser);
-    }
-    else{
+      } else {
         console.log("EL PASSWORD NO COINCIDE");
         return done(null, false);
+      }
     }
-}))
+  )
+);
 
-
-passport.use("login", new LocalStrategy({
-    usernameField: "username",
-    passwordField: "password",
-    passReqToCallback: true
-    }, async (res: Response, username: string, password: string, done: any) =>{
-        const db = await connect();
-        const verifUser: any = await db.query("SELECT * FROM users WHERE username = ?", [username]);
-        if(verifUser.length > 0){
-            const user = verifUser[0]
-            const verifiPassword = await decryptPassword(password, user[0].password);
-            if(verifiPassword){
-                done(null, user[0]);
-            }
-            else{
-                console.log("alerta de ese usuario no existe")
-                //alerta de password incorrecto
-                done(null, false);
-            }
+passport.use(
+  "login",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (res: Response, username: string, password: string, done: any) => {
+      const db = await connect();
+      const verifUser: any = await db.query(
+        "SELECT * FROM users WHERE username = ?",
+        [username]
+      );
+      if (verifUser.length > 0) {
+        const user = verifUser[0];
+        const verifiPassword = await decryptPassword(
+          password,
+          user[0].password
+        );
+        if (verifiPassword) {
+          done(null, user[0]);
+        } else {
+          done(null, false);
         }
-        else{
-            //alerta de ese usuario no existe
-            console.log("alerta de ese usuario no existe")
-            done(null, false);
-        }
-        
+      } else {
+        done(null, false);
+      }
     }
-))
-
+  )
+);
 
 passport.serializeUser((user: any, done: any) => {
-    done(null, user.id);
-  });
+  done(null, user.id);
+});
 
 passport.deserializeUser(async (id: number, done: any) => {
-    const db = await connect();
-    const rows = await db.query('SELECT * FROM users WHERE id = ?', [id]);
-    done(null, rows[0]);
+  const db = await connect();
+  const rows = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+  done(null, rows[0]);
 });
--- src/public/javascript/main/options.js
+++ src/public/javascript/main/options.js
@@ -1,188 +1,169 @@
-$(document).ready( () =>{
-    
-    $("#windowCreatePublication").click( () => {
-        $('#navigation').show();
-        $("#divBlurid").hide();
-        $("#boxCreatePublication").hide();
-        $(".preview").hide();
-        $(".optionViewPreview").hide();
-        $(".controlsVideo").hide();
-        $(".divClosePublication").hide();
-        $("#descriptionNewPost").text("");
+$(document).ready(() => {
+  $("#windowCreatePublication").click(() => {
+    $("#navigation").show();
+    $("#divBlurid").hide();
+    $("#boxCreatePublication").hide();
+    $(".preview").hide();
+    $(".optionViewPreview").hide();
+    $(".controlsVideo").hide();
+    $(".divClosePublication").hide();
+    $("#descriptionNewPost").text("");
+  });
 
-    });
+  $("#windowOptionUserFriendChat").click(() => {
+    $("#divBlurid").hide();
+    $("#boxOptionsUserFriendChat").hide();
+    $("#navigation").show();
+  });
 
-    $("#windowOptionUserFriendChat").click( () => {
-        $("#divBlurid").hide();
-        $("#boxOptionsUserFriendChat").hide();
-        $('#navigation').show();
-    });
+  $(".userPhotoChat").click(() => {
+    $("#divBlurid").show();
+    $("#boxOptionsUserFriendChat").show();
+    $("#navigation").hide();
+  });
 
-    $(".userPhotoChat").click( () => {
-        $("#divBlurid").show();
-        $("#boxOptionsUserFriendChat").show();
-        $('#navigation').hide();
-    });
+  $("#eliminarFileCreatePublication").click(() => {
+    $(".preview").hide();
+    $(".divClosePublication").hide();
+    $(".optionViewPreview").hide();
+    $(".controlsVideo").hide();
+  });
 
-    $("#eliminarFileCreatePublication").click( () => {
-        $(".preview").hide();
-        $(".divClosePublication").hide();
-        $(".optionViewPreview").hide();
-        $(".controlsVideo").hide();
-        
-    })
+  $("#selectImagePublication").click(() => {
+    $("#selectVideoPublication").val("");
+    $("#selectImagePublication").val("");
+    $(".preview").css("border-radius", "10px");
+    $(".preview").hide();
+    $(".controlsVideo").hide();
+    $("video").get(0).pause();
+    $(".optionViewPreview").hide();
+    $("#viewVideo").hide();
+    $("#viewAudio").hide();
+    $(".divClosePublication").hide();
+    viewImage();
+  });
 
+  $("#selectVideoPublication").click(() => {
+    $("#selectImagenPublication").val("");
+    $("video").get(0).pause();
+    $("#selectVideoPublication").val("");
+    $(".preview").hide();
+    $(".optionViewPreview").hide();
+    $(".controlsVideo").hide();
+    $("#viewImagen").hide();
+    $("#viewAudio").hide();
+    $(".divClosePublication").hide();
+  });
 
+  $("#selectAudioPublication").click(() => {
+    $("#selectImagenPublication").val("");
+    $("#selectVideoPublication").val("");
+    $("video").get(0).pause();
+    audio.pause();
+    $(".preview").css("border-radius", "10px");
+    $("#selectAudioPublication").val("");
+    $(".optionViewPreview").hide();
+    $(".controlsVideo").hide();
+    $("#viewVideo").hide();
+    $("#viewImagen").hide();
+    $(".preview").show();
+    $("#viewAudio").show();
+    $(".divClosePublication").hide();
+  });
 
-    $("#selectImagePublication").click( () => {
-        $('#selectVideoPublication').val("");
-        $('#selectImagePublication').val("");
-        $(".preview").css('border-radius','10px');
-        $(".preview").hide();
-        $(".controlsVideo").hide();
-        $("video").get(0).pause();
-        $(".optionViewPreview").hide();
-        $("#viewVideo").hide();
-        $("#viewAudio").hide();
-        $(".divClosePublication").hide();
-        viewImage();
-    })
-    
-    $("#selectVideoPublication").click( () => {
-        $('#selectImagenPublication').val("");
-        $("video").get(0).pause();
-        $('#selectVideoPublication').val("");
-        $(".preview").hide();
-        $(".optionViewPreview").hide();
-        $(".controlsVideo").hide();
-        $("#viewImagen").hide();
-        $("#viewAudio").hide();
-        $(".divClosePublication").hide();
-    })
+  $("#sendPublication").click(() => {
+    $("#navigation").show();
+    $(".optionViewPreview").hide();
+    $("#boxCreatePublication").hide();
+  });
 
-    $("#selectAudioPublication").click( () => {
-        $('#selectImagenPublication').val("");
-        $('#selectVideoPublication').val("");
-        $("video").get(0).pause();
-        audio.pause();
-        $(".preview").css('border-radius','10px');
-        $('#selectAudioPublication').val("");
-        $(".optionViewPreview").hide();
-        $(".controlsVideo").hide();
-        $("#viewVideo").hide();
-        $("#viewImagen").hide();
-        $(".preview").show();
-        $("#viewAudio").show();
-        $(".divClosePublication").hide();
-        
-    })
+  $("#modeSun").click(() => {
+    $("#modeSun").hide();
+    $("#modeMoon").show();
+    /*$("*").css({
+      $white: "#000",
+      $lowlowWhite: "#333333",
+      $lowWhite: "#191919",
+      $backgroundComponent: "#fff",
+      $colorActiveBtn: "#b6b3b3",
+      $borderBlack: "#cccccc",
+      $backgrounBody: "#e6e6e6",
+      $backgroundScroll: "#e6e6e6",
+      $btnInteractionPost: "#d6d2d2a1",
+      $colorIconsHeadMenu: "#000",
+      $backgrounWindow: "#fff",
+      $actionBtnDefaul: "#B9b5b5",
+      $leftoverBoxDark: "#7b7b7b",
+      $boderBlurBox: "rgba(0, 0, 0, 0.1)",
+      $backgroundFile:
+        "radial-gradient(circle at 0% 0%," +
+        "#dcdacf 50%, #b1b1b8 66.67%, #8c8ca3 83.33%, #6e6b8f 100%)",
+      $backgroundNavigation: "rgba(255, 255, 255, 0.68)",
+    });*/
+  });
 
+  $("#modeMoon").click(() => {
+    $("#modeMoon").hide();
+    $("#modeSun").show();
+    /*document.documentElement.style.setProperty({
+      $white: "#fff",
+      $lowlowWhite: "#b6b6b6",
+      $lowWhite: "#e7e7e7",
+      $backgroundComponent: "#050505",
+      $colorActiveBtn: "#000000",
+      $borderBlack: "#0d0d0d",
+      $backgrounBody: "#1a1a1a",
+      $backgroundScroll: "#101113",
+      $btnInteractionPost: "#070707a1",
+      $colorIconsHeadMenu: "#fff",
+      $backgrounWindow: "#000",
+      $actionBtnDefaul: "#1f1f1f",
+      $leftoverBoxDark: "#101113",
+      $boderBlurBox: "rgba(255, 255, 255, 0.1)",
+      $backgroundFile:
+        "radial-gradient(circle at 0% 0%," +
+        "#313130 50%, #222127 66.67%, #15131e 83.33%, #080016 100%)",
+      $backgroundNavigation: "rgba(0, 0, 0, 0.68)",
+    });*/
+  });
 
-    $("#sendPublication").click(() =>{
-        $("#navigation").show();
-        $(".optionViewPreview").hide();
-        $("#boxCreatePublication").hide();
-   })
+  $("#btnCommentPublication").click(() => {
+    $("#navigation").hide();
+    let scrollLocation = window.pageYOffset;
+    window.onscroll = function () {
+      let scrollUser = window.pageYOffset;
+      if (scrollLocation >= scrollUser) {
+        document.getElementById("inputCommentUser").style.display = "block";
+      } else {
+        document.getElementById("inputCommentUser").style.display = "none";
+      }
+      scrollLocation = scrollUser;
+    };
+  });
 
+  $(".btnClosrMessage").click(() => {
+    $(".content_message").hide();
+  });
+});
 
-    $("#modeSun").click( () =>{
-        $("#modeSun").hide();
-        $("#modeMoon").show();
-        $("*").css({
-            "$white": "#000",
-            "$lowlowWhite": "#333333",
-            "$lowWhite": "#191919",
-            "$backgroundComponent": "#fff",
-            "$colorActiveBtn": "#b6b3b3",
-            "$borderBlack": "#cccccc",
-            "$backgrounBody": "#e6e6e6",
-            "$backgroundScroll": "#e6e6e6",
-            "$btnInteractionPost": "#d6d2d2a1",
-            "$colorIconsHeadMenu": "#000",
-            "$backgrounWindow": "#fff",
-            "$actionBtnDefaul": "#B9b5b5",
-            "$leftoverBoxDark": "#7b7b7b",
-            "$boderBlurBox": "rgba(0, 0, 0, 0.1)",
-            "$backgroundFile": "radial-gradient(circle at 0% 0%,"+
-                "#dcdacf 50%, #b1b1b8 66.67%, #8c8ca3 83.33%, #6e6b8f 100%)",
-            "$backgroundNavigation": "rgba(255, 255, 255, 0.68)"
-        });
-        
-    })
-
-    $("#modeMoon").click( () =>{
-        $("#modeMoon").hide();
-        $("#modeSun").show();
-        document.documentElement.style.setProperty({
-            "$white": "#fff",
-            "$lowlowWhite": "#b6b6b6",
-            "$lowWhite": "#e7e7e7",
-            "$backgroundComponent": "#050505",
-            "$colorActiveBtn": "#000000",
-            "$borderBlack": "#0d0d0d",
-            "$backgrounBody": "#1a1a1a",
-            "$backgroundScroll": "#101113",
-            "$btnInteractionPost": "#070707a1",
-            "$colorIconsHeadMenu": "#fff",
-            "$backgrounWindow": "#000",
-            "$actionBtnDefaul": "#1f1f1f",
-            "$leftoverBoxDark": "#101113",
-            "$boderBlurBox": "rgba(255, 255, 255, 0.1)",
-            "$backgroundFile":  "radial-gradient(circle at 0% 0%,"+ 
-                "#313130 50%, #222127 66.67%, #15131e 83.33%, #080016 100%)",
-            "$backgroundNavigation": "rgba(0, 0, 0, 0.68)"
-        });
-    })
-
-
-
-    $("#btnCommentPublication").click( () =>{
-        $('#navigation').hide();
-        let scrollLocation  = window.pageYOffset;
-        window.onscroll = function() {
-        let scrollUser = window.pageYOffset;
-        if(scrollLocation >= scrollUser){
-            document.getElementById('inputCommentUser').style.display = 'block';
-        }
-        else{
-            document.getElementById('inputCommentUser').style.display = 'none';
-        }
-        scrollLocation = scrollUser;
-    }
-    })
-
-    $(".btnClosrMessage").click( () =>{
-        $(".content_message").hide();
-    })
-
-})
-
-
-
-
-function viewImage(){
-    const $seleccionArchivos = document.querySelector("#selectImagePublication"),
+function viewImage() {
+  const $seleccionArchivos = document.querySelector("#selectImagePublication"),
     $imagenPrevisualizacion = document.querySelector("#viewImagen");
 
-    $seleccionArchivos.addEventListener("change", () => {
-
+  $seleccionArchivos.addEventListener("change", () => {
     const archivos = $seleccionArchivos.files;
 
-    
     $(".preview").show();
     $("#viewVideo").hide();
     $("#viewImagen").show();
     $(".divClosePublication").show();
 
-
     if (!archivos || !archivos.length) {
-        $imagenPrevisualizacion.src = "";
-        return;
+      $imagenPrevisualizacion.src = "";
+      return;
     }
     const primerArchivo = archivos[0];
     const objectURL = URL.createObjectURL(primerArchivo);
     $imagenPrevisualizacion.src = objectURL;
-    });
+  });
 }
-
-
